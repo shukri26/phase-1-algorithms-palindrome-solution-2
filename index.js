5fc8178eca225b73eee7b68aabcdd3b314f77cb9
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-09 22:01:08 +0300

    complete

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+const chai = require("chai");
+global.expect = chai.expect;
+const isPalindrome = require("../index");
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+describe("isPalindrome", () => {
+  it("returns true for 'abba'", () => {
+    expect(isPalindrome("abba")).to.be.true;
+  });
+  it("returns true for 'racecar'", () => {
+    expect(isPalindrome("racecar")).to.be.true;
+  });
+  it("returns true for 'a'", () => {
+    expect(isPalindrome("a")).to.be.true;
+  });
+  it("returns false for 'robot'", () => {
+    expect(isPalindrome("robot")).to.be.false;
+  });
+  it("returns false for 'ab'", () => {
+    expect(isPalindrome("ab")).to.be.false;
+  });
+});
